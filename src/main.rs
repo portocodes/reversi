3fad6c035f65c3e637b5a5d9e18bf66cfc7b5d44
Ricardo Mendes <rokusu@gmail.com>
2015-10-02 04:43:35 +0000

    f

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate reversi;
+
+use std::io;
+use std::io::Write;
+use reversi::{Board};
+
+fn main() {
+    let mut stdin = io::stdin();
+    let input = &mut String::new();
+    let mut board = Board::default();
+    
+
+    //while !board.finished() {
+    loop {
+        input.clear();
+        println!("{}", board);
+        print!("Player 1> ");
+        io::stdout().flush();
+        stdin.read_line(input);
+        
+        if input == "sext\n" {
+            std::process::exit(0)
+        };
+        
+        let coordinates: Vec<&str> = input.trim().split(",").collect();
+        let y = usize::from_str_radix(coordinates.first().unwrap(), 10).unwrap() - 1;
+        let x = usize::from_str_radix(coordinates.last().unwrap(), 10).unwrap() - 1;
+        println!("-> {},{} <-", x,y);
+        
+        let blurp = board.make_move(x, y);
+        
+        match blurp {
+            Ok(c) => println!("{:?}", c),
+            _ => {}
+        };
+
+        // read input
+        // make move
+        // print result
+    }
+}
