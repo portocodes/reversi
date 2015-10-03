cfcf3e25ed995db630a3991ae920210749b42d19
Hugo Peixoto <hugo.peixoto@gmail.com>
2015-10-03 03:06:39 +0000

    Reads x,y from input instead of y,x

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let mut stdin = io::stdin();
     let input = &mut String::new();
     let mut board = Game::default();
-    
 
 
     while !board.finished() {
@@ -18,7 +17,7 @@ fn main() {
         io::stdout().flush();
         stdin.read_line(input);
         println!("");
-        
+
         if input == "" {
             println!("Exiting...");
             std::process::exit(0);
@@ -26,22 +25,18 @@ fn main() {
         if input == "sext\n" {
             break
         };
-        
+
         let coordinates: Vec<&str> = input.trim().split(",").collect();
-        let y = usize::from_str_radix(coordinates.first().unwrap(), 10).unwrap() - 1;
-        let x = usize::from_str_radix(coordinates.last().unwrap(), 10).unwrap() - 1;
+        let x = usize::from_str_radix(coordinates.first().unwrap(), 10).unwrap() - 1;
+        let y = usize::from_str_radix(coordinates.last().unwrap(), 10).unwrap() - 1;
         println!("-> {},{} <-", x,y);
-        
+
         let blurp = board.make_move(x, y);
-        
+
         match blurp {
             Ok(c) => println!("{:?}", c),
             _ => {}
         };
-
-        // read input
-        // make move
-        // print result
     }
 
     let score = board.score();
